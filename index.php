9c004ffd8145431e659be9cfcdfa6665940b5031
Lexi Ross <lexiberylross@gmail.com>
2013-01-23 22:00:59 -0500

    basic site structure

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>HackHarvard Knowledge Base</title>
+        <meta charset="utf-8">
+        <link href="styles.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div id="header">HackHarvard Knowledge Base</div>
+        <div id="container">
+            <div id="left-bar">
+                <ul id="list">
+                </ul>
+            </div>
+            <div id="right-panel">
+                <div id="pdf-viewer">
+                </div>
+                <div id="comments">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
